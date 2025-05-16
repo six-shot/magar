c13a4ab00cf5f11d217f973d524dba58e0b70842
six-shot <okhuomonajayi54@gmail.com>
2025-05-16 12:42:42 +0100

    petition

--- src/modules/home/features/hero/index.jsx
+++ src/modules/home/features/hero/index.jsx
@@ -6,24 +6,24 @@ import { Link } from "react-router-dom";
 
 export default function Hero() {
   return (
-    <div className="relative h-screen ">
-      <div className="w-full">
-        <div className="absolute top-[250px] w-full z-10 text-center pointer-events-none">
-          <div className="w-full h-[848px] relative -top-[170px] flex justify-center items-center">
-            <h1 className="absolute bottom-[350px] md:text-[160px] text-[60px] -z-10 font-semibold hero-text text-center">
-              Personalized <br /> Experience
-            </h1>
-          </div>
+    <div className="relative h-screen  ">
+      <div className="absolute bottom-[15%] w-full z-10 h-full pointer-events-none px-[5%]">
+        <div className="w-full h-full flex flex-col justify-end pb-8">
+          <h1 className="text-white font-bold text-[45px] leading-[50px]">
+            Simplifying Crypto <br />
+            Transactions
+          </h1>
+          <p className="text-white font-medium sm:text-lg text-base sm:w-[500px] w-full mt-[3%] mb-4">
+            An ERC-4337 toolkit enhancing gasless transactions, social login,
+            multichain support and much more making DeFi accessibile and secure.
+          </p>
         </div>
-
-        <iframe
-          src="https://my.spline.design/newton39scradle-bFSdhC7jVplvT8X38oHE4lnd/"
-          frameBorder="0"
-          width="100%"
-          height="100%"
-          className="absolute z-0"
-        ></iframe>
       </div>
+
+      <Spline
+        className="absolute  z-0"
+        scene="https://prod.spline.design/HXtrvfpheoREhBFV/scene.splinecode"
+      /> */
     </div>
   );
 }
