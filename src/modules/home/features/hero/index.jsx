cd5bd77f2c191f0ceefc996a33f0f07cca2a254f
six-shot <okhuomonajayi54@gmail.com>
2025-05-16 14:10:47 +0100

    fixes

--- src/modules/home/features/hero/index.jsx
+++ src/modules/home/features/hero/index.jsx
@@ -7,7 +7,7 @@ import { Link } from "react-router-dom";
 export default function Hero() {
   return (
     <div className="relative h-screen ">
-      <div className="absolute bottom-[15%] w-full z-10 h-full pointer-events-none  ">
+      <div className="absolute bottom-[15%] w-full z-10 h-full  ">
         <div className="w-full h-full flex flex-col justify-end pb-8 max-w-[1440px] mx-auto px-[5%]">
           <h1 className="text-white font-bold text-[45px] leading-[50px]">
             Simplifying Crypto <br />
@@ -23,7 +23,13 @@ export default function Hero() {
         className="absolute  z-0"
         scene="https://prod.spline.design/HXtrvfpheoREhBFV/scene.splinecode"
       />{" "} */}
-      */
+      <iframe
+        src="https://my.spline.design/newton39scradle-bFSdhC7jVplvT8X38oHE4lnd/"
+        frameBorder="0"
+        width="100%"
+        height="100%"
+        className="absolute z-0"
+      ></iframe>
     </div>
   );
 }
